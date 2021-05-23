6f4d7b79a4d348025e62e773424c306a185891c9
Sunny Varanasi <sunny@hatchet.com.au>
2021-05-23 20:41:06 +0800

    üser 2 new c

--- fetchTest.vue
+++ fetchTest.vue
@@ -1,8 +1,6 @@
 new file to check fdetch command
 code 2
 user 1 changed
-<<<<<<< HEAD
 user 2 correct change
-=======
 user i chnage but wrong
->>>>>>> fe2d8f76101fef534a27361d332123508a1c267d
+new 1
